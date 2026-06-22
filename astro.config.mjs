4f7939e7e87f14718563fac3a59844e1367f07c9
Wlad Paiva <desenvolvedormg@gmail.com>
2026-06-22 20:31:49 -0300

    add varlock

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import varlockAstroIntegration from '@varlock/astro-integration';
+import { varlockCloudflareVitePlugin } from '@varlock/cloudflare-integration';
 
 // https://astro.build/config
 export default defineConfig({
+    // this builds with an error
     adapter: cloudflare(),
+    integrations: [varlockAstroIntegration()],
+
+    // this fails as well
+    // vite: {
+    //     plugins: [varlockCloudflareVitePlugin()]
+    // }
 });
